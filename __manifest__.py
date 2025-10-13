29c49a5ae4655d2d5b755f9daf1a1c9284ce5c37
Cesar Mosquera <julionmosquera252@gmail.com>
2025-10-13 12:00:00 -0500

    COMIT 3

--- __manifest__.py
+++ __manifest__.py
@@ -22,15 +22,18 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-         'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         #'views/templates.xml',
     ],
     # only loaded in demonstration mode
     #'demo': [
-    #    'demo/demo.xml',
+     #   'demo/demo.xml',
     #],
     'application': True,
-    'license': 'AGPL-3',
+    'license': 'AGPL-3'
 }
+
+#12.5.0 Nos indica que cambio ha tenido el software, el tercero, son cosas pequeñas tipo errores ortograficos, errores que habia, el segundo es el cambio de la lógica, y el primero es un cambio mucho mas robuzto que cambia toda la estructura.
+#Depende de donde este el cambio, se hacen los test, si el tercero lo actualizo nomas, el segundo depende pero no hay que ser muchas pruebas el primero si o si toca hacer pruebas robustas
 
--- models/models.py
+++ models/models.py
@@ -1,18 +1,48 @@
-# -*- coding: utf-8 -*-
- from odoo import models, fields, api
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
 
 
- class biblioteca(models.Model):
-     _name = 'biblioteca.libro'
-     _description = 'biblioteca.biblioteca'
+class biblioteca_libro(models.Model):
+    _name = 'biblioteca.libro'
+    _description = 'biblioteca.biblioteca' 
+    
+    firstname = fields.Char(string="Nombre Libro")
+    autor = fields.Many2one('biblioteca.autor', string='Autor Libro')
+    value = fields.Integer(string='Numero ejemplares')
+    publication_date = fields.Integer(string="Año de publicación")
+    value2 = fields.Float(compute="_value_pc", store=True, string='Costo') #---- Se guarda en la base de datos 
+    isbn = fields.Integer(string="isbn")
+    n_paginas = fields.Integer(string="Num Paginas")
+    description = fields.Text(string='Resumen Libro')
+    genero = fields.Text(string="Género/Categoría")
+    estado = fields.Selection([
+        ('disponible', 'Disponible'),
+        ('reservado', 'Reservado'),
+        ('prestado', 'Prestado')
+    ], string="Estado", default='disponible')
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
 
-     @api.depends('value')
-     def _value_pc(self):
-         for record in self:
-             record.value2 = float(record.value) / 100
-
+class BibliotecaAutor(models.Model):
+    _name = 'biblioteca.autor'
+    _description = 'biblioteca.autor'
+    
+    #autor_id = fields.integer(string="autor_id") algo no sireve
+    firstname = fields.Char()
+    lastname = fields.Char()
+    
+    @api.depends('firstname','lastname')
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = f"{record.firstname} {record.lastname}"
+    
+class BibliotecaEditorial(models.Model):
+    _name = 'biblioteca.editorial'
+    _description = 'biblioteca.editorial'
+    
+    firstname = fields.Char(string="Nombre editorial")
+   
